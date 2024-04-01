298390495500e95059b735e19fcdccc1985cb3a5
roniqueh <roniquehossain@gmail.com>
2024-04-01 17:39:50 +0100

    Add terminal split

--- init.lua
+++ init.lua
@@ -175,6 +175,9 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- Set split terminal keymap
+vim.keymap.set('n', '<leader>t', ':vsplit terminal<cr>', { desc = 'Split [T]erminal' })
+
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
