36ea6b92ce18efefd53ecfe1da5388d33a9bade4
roniqueh <roniquehossain@gmail.com>
2024-05-08 20:23:47 +0100

    Update

--- init.lua
+++ init.lua
@@ -164,9 +164,11 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = false
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Keep cursor vertically centered for half-page jumps
-vim.keymap.set('n', '<C-u>', '<C-u>^zz', { desc = 'Half page up' })
+-- Keep cursor vertically centered for vertical movement
+vim.keymap.set('n', 'j', 'jzz', { desc = 'Half page up' })
+vim.keymap.set('n', 'k', 'kzz', { desc = 'Half page up' })
 vim.keymap.set('n', '<C-d>', '<C-d>^zz', { desc = 'Half page down' })
+vim.keymap.set('n', '<C-u>', '<C-u>^zz', { desc = 'Half page up' })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
@@ -752,16 +754,82 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    'folke/tokyonight.nvim',
+    'rose-pine/neovim',
+    config = function()
+      require('rose-pine').setup {
+        dim_inactive_windows = true,
+        extend_background_behind_borders = true,
+
+        enable = {
+          terminal = true,
+          legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+          migrations = true, -- Handle deprecated options automatically
+        },
+
+        styles = {
+          bold = true,
+          italic = true,
+          transparency = false,
+        },
+
+        groups = {
+          border = 'muted',
+          link = 'iris',
+          panel = 'surface',
+
+          error = 'love',
+          hint = 'iris',
+          info = 'foam',
+          note = 'pine',
+          todo = 'rose',
+          warn = 'gold',
+
+          git_add = 'foam',
+          git_change = 'rose',
+          git_delete = 'love',
+          git_dirty = 'rose',
+          git_ignore = 'muted',
+          git_merge = 'iris',
+          git_rename = 'pine',
+          git_stage = 'iris',
+          git_text = 'rose',
+          git_untracked = 'subtle',
+
+          h1 = 'iris',
+          h2 = 'foam',
+          h3 = 'rose',
+          h4 = 'gold',
+          h5 = 'pine',
+          h6 = 'foam',
+        },
+
+        highlight_groups = {
+          Comment = { fg = 'foam' },
+          VertSplit = { fg = 'muted', bg = 'muted' },
+        },
+
+        before_highlight = function(group, highlight, palette)
+          -- Disable all undercurls
+          -- if highlight.undercurl then
+          --   highlight.undercurl = false
+          -- end
+          --
+          -- Change palette colour
+          if highlight.fg == palette.pine then
+            highlight.fg = palette.foam
+          end
+        end,
+      }
+    end,
     priority = 1000, -- make sure to load this before all the other start plugins
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'rose-pine-moon'
 
       -- You can configure highlights by doing something like
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
 
